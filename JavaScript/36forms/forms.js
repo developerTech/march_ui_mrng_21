f4f035fe68d0e08e950d3823c3dc61793af5c8d2
aakashdeveloper <ahanda205@gmail.com>
2022-04-28 09:43:33 +0530

    froms

---
+++ JavaScript/36forms/forms.js
@@ -0,0 +1,71 @@
+const getId = () => {
+    let id = Math.floor(Math.random() * 1000000)
+    document.getElementById('eid').value = `EAZ${id}`
+}
+
+function validateFname(){
+    let name = document.getElementById('firstname').value;
+    let fdiv = document.getElementById('fdiv');
+    if(name.trim().length == 0){
+        document.getElementById('fout').innerText = 'Please Enter First Name'
+        fdiv.classList.remove('has-success')
+        fdiv.classList.add('has-error')
+    }else{
+        document.getElementById('fout').innerText = ''
+        fdiv.classList.remove('has-error')
+        fdiv.classList.add('has-success')
+    }
+}
+
+const validateEmail = () => {
+    let email = document.getElementById('email').value;
+    if(email.trim().length == 0){
+        document.getElementById('eout').innerText = 'Please Enter Email'
+    }else{
+        if(email.match("^([a-z0-9A-Z]+)@([a-zA-Z0-9]+)\.([a-zA-Z]{2,5})$") == null){
+            document.getElementById('eout').innerText = 'Please Enter Valid Email'
+        }else{
+            document.getElementById('eout').innerText = ''
+        }
+    }
+}
+
+function validatePassword(){
+    let pwd = document.getElementById('password').value;
+    if(pwd.trim().length < 15){
+        if(pwd.trim().length < 8){
+            document.getElementById('pout').innerText = 'Min Length is 8'
+            document.getElementById('ppout').style.display="block";
+            document.getElementById('ppout').style.backgroundColor="red"
+        }else if(pwd.trim().length > 8 && pwd.trim().length < 11){
+            document.getElementById('pout').innerText = ''
+            document.getElementById('ppout').style.backgroundColor="orange"
+        }
+    }else{
+        document.getElementById('ppout').style.backgroundColor="green"
+    }
+}
+
+const showPassword = () => {
+    let pwd = document.getElementById('password');
+    if(pwd.type == 'password'){
+        pwd.type = "text"
+    }else{
+        pwd.type = "password"
+    }
+}
+
+function validateCPassword(){
+    let pwd = document.getElementById('password').value;
+    let cpwd = document.getElementById('cpassword').value;
+    if(pwd !== cpwd){
+        document.getElementById('cpout').innerText = 'Password does not match'  
+    }else{
+        document.getElementById('cpout').innerText = ''
+    }
+}
+
+const getCity = () => {
+    let city = document.getElementById('city').value;
+    document.getElementById('cityOut').innerText = `Your City is ${city}`
+}
