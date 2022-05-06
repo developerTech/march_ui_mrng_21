3ec357a08c513d9ca9f914cdeb5e25799d8c0c54
aakashdeveloper <ahanda205@gmail.com>
2022-05-06 09:34:03 +0530

    ajax

---
+++ JavaScript/48sets.js
@@ -0,0 +1,16 @@
+let set1 = new Set([10,20,30,40])
+
+set1
+Set(4) {10, 20, 30, 40}
+let getEntry = set1.entries()
+undefined
+getEntry.next().value
+(2) [10, 10]
+getEntry.next().value
+(2) [20, 20]
+getEntry.next().value
+(2) [30, 30]
+getEntry.next().value
+(2) [40, 40]
+getEntry.next().value
+undefined
